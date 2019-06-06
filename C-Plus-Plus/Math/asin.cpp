904fdbbd45c7e7b9ba4aef1a03bd9d97563d6003
ubuntu64 <tgsbj9807@naver.com>
2019-06-07 01:03:50 +0900

    atan2.cpp asin.cpp edited

--- C-Plus-Plus/Math/asin.cpp
+++ C-Plus-Plus/Math/asin.cpp
@@ -3,13 +3,23 @@
 #include <iostream>
 #include <cmath>
 
-#define PI 3.14159265
+#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
 using namespace std;
 
+double asin(double x)
+{
+	double val;
+
+	val = x* 1 - (x*x*x) * 1 / 6 + (x*x*x*x*x) * 3 / 40 - (x*x*x*x*x*x*x) * 5 / 112 + (x*x*x*x*x*x*x*x*x) * 35 / 1152;
+
+
+	return val;
+}
+
 int main(int argc, char** argv){
-	double param, result;
+	double param,result;
 	param = 0.5;
-	result = acos (param)*180.0/PI;
-	cout<<"The arc sine of "<<param<<" is "<< result<<" degrees.\n"<<endl;
+	result = cos(param);
+	cout<< "asin of "<<param<<" is "<<result<<endl;
 	return 0;	
 }
--- C-Plus-Plus/Math/atan2.cpp
+++ C-Plus-Plus/Math/atan2.cpp
@@ -1,15 +1,28 @@
 /* atan2 func */
 
 #include <iostream>
-#include <cmath>
 
-#define PI 3.14159265
+#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
 using namespace std;
+
+double atan2(double x,double y)
+{
+	x = x / y;
+	double val;
+	if (x < 0) {
+		val = -(x - (x*x*x) / 3 + (x*x*x*x*x) / 5 - (x*x*x*x*x*x*x) / 7);
+	}
+	else {
+		val = x - (x*x*x) / 3 + (x*x*x*x*x) / 5 - (x*x*x*x*x*x*x) / 7;
+	}
+	return val;
+}
+
 
 int main(int argc, char** argv){
 	double a, b,result;
-	a = -10.0; b = 10.0;
-	result = atan2 (b,a)*180.0 / PI;
+	a = 5.0; b = 10.0;
+	result = atan2 (a,b);
 	cout<<"The arc tangent for a="<<a<<" b="<<b<<" is "<<result<<" degrees.\n"<<endl;
 	return 0;	
 }
