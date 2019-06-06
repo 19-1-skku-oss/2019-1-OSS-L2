4a4fc295ef82cccc967ac9f4c0b17f6143904d89
ubuntu64 <tgsbi9807@naver.com>
2019-06-07 00:03:50 +0900

    acos.cpp edited

--- C-Plus-Plus/Math/acos.cpp
+++ C-Plus-Plus/Math/acos.cpp
@@ -1,15 +1,30 @@
 /* acos func*/
 
 #include <iostream>
-#include <cmath>
 
-#define PI 3.14159265
 using namespace std;
+#define PI 3.14159265
 
-int main(int argc, char** argv){
-	double param, result;
+double atan(double x)
+{
+	double val;
+	if (x < 0) {
+		val = -(x - (x*x*x) / 3 + (x*x*x*x*x) / 5 - (x*x*x*x*x*x*x) / 7);
+	}
+	else if (x > 1) {
+		val = PI / 2 - atan(1 / x);
+		
+	}
+	else {
+		val = x - (x*x*x) / 3 + (x*x*x*x*x) / 5 - (x*x*x*x*x*x*x) / 7;
+	}
+	return val;
+}
+
+int main(int argc,char**argv)
+{
+	double param,result;
 	param = 0.5;
-	result = acos (param)*180.0/PI;
-	cout<<"The arc cosine of "<<param<<" is "<< result<<" degrees.\n"<<endl;
-	return 0;	
+	result = atan(param);
+	cout<< "atan of "<<param<<" is "<<result<<endl;
 }
