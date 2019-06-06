e49ed66bd37846765fac7caff6bccc01e857ddf5
ubuntu64 <tgsbj9807@naver.com>
2019-06-07 01:55:41 +0900

    added e_pow_x.cpp log.cpp

---
+++ C-Plus-Plus/Math/log.cpp
@@ -0,0 +1,22 @@
+/* acos func*/
+#include <iostream>
+
+using namespace std;
+#define PI 3.141592653589793238462643383279502884197169
+
+double log(double x)
+{
+	double val;
+	val = (x - 1) - (x - 1)*(x - 1) / 2 + (x - 1)*(x - 1)*(x - 1) / 3 - (x - 1)*(x - 1)*(x - 1)*(x - 1) / 4 + (x - 1)*(x - 1)*(x - 1)*(x - 1)*(x - 1) / 5 - (x - 1)*(x - 1)*(x - 1)*(x - 1)*(x - 1)*(x - 1) / 6;
+
+	return val;
+}
+
+int main(int argc,char**argv)
+{
+	double param,result;
+	param = 2;
+	result = log(param);
+	cout<< "log of "<<param<<" is "<<result<<endl;
+	return 0;
+}
