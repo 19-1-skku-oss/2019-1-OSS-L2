2d78ca55a5946e6727925edc063fa41812f87913
ljw322 <30587502+ljw322@users.noreply.github.com>
2019-06-07 01:36:01 +0900

    Update sqrt.cpp

--- C-Plus-Plus/Math/sqrt.cpp
+++ C-Plus-Plus/Math/sqrt.cpp
@@ -7,7 +7,7 @@ double SQRT(double, double);
 
 double SQRT(double input, double x)
 {
-	for (int i = 0; i < 10; i++) { x = (x + (input / x)) / 2; }    // 이 한줄 땜에 주말내내 머리에 쥐남 ㅠㅠ
+	for (int i = 0; i < 10; i++) { x = (x + (input / x)) / 2; }    
 
 	return x;
 }
