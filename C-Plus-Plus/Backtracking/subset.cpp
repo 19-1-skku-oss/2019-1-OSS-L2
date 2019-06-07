1e82f083b1002266c0f433259f2e662c9cccf23f
ubuntu64 <tgsbj9807@naver.com>
2019-06-07 20:42:48 +0900

    fixed datatype problem

--- C-Plus-Plus/Backtracking/subset.cpp
+++ C-Plus-Plus/Backtracking/subset.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-vector<int> find_match_case(int match,vector<int> arr){
+vector<vector<int>> find_match_case(int match,vector<int> arr){
 	int nelement = 0;	//number of subset element
-	//cout<<"i am here"<<endl;
-	vector< <vector<int> > total_subset;
+	vector< vector<int> > total_subset;
+	
 	for(int i=0; i < (1 << (arr.size())); i++){
 		vector<int> subset;
 		for(int j=0;j<arr.size();j++){
@@ -25,7 +25,7 @@ vector<int> find_match_case(int match,vector<int> arr){
 int main(int argc,char** argv){
 	int N=5,S=0;
 	vector<int> arr{-7,-3,-2,5,8};
-	vector<int> result;
+	vector<vector<int>> result;
 	result = find_match_case(0,arr);
 	for(int i=0;i<result.size();i++){
 		cout<<"num of"<< i<<" element case"<<endl;
