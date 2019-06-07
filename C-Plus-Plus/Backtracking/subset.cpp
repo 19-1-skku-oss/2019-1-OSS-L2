6e8093f540c0381b2f4881a196fa29ae2e1ef702
ubuntu64 <tgsbj9807@naver.com>
2019-06-07 20:51:18 +0900

    fixed segfalut problem

--- C-Plus-Plus/Backtracking/subset.cpp
+++ C-Plus-Plus/Backtracking/subset.cpp
@@ -4,17 +4,18 @@
 using namespace std;
 
 vector<vector<int>> find_match_case(int match,vector<int> arr){
-	int nelement = 0;	//number of subset element
+	int nelement = arr.size();	//number of subset element
 	vector< vector<int> > total_subset;
 	
-	for(int i=0; i < (1 << (arr.size())); i++){
+	for(int i=0; i < (1 << (nelement)); i++){
 		vector<int> subset;
-		for(int j=0;j<arr.size();j++){
-			if (i & (1<<j))
+		for(int j=0;j<nelement;j++){
+			if (i & (1<<j)){
 				subset.push_back(arr[j]);
+			}
 		}
 		int check=0;
-		for(int k=0;k<arr.size();k++){
+		for(int k=0;k<subset.size();k++){
 			check+=subset[k];
 		}
 		if(check==match)
@@ -26,13 +27,14 @@ int main(int argc,char** argv){
 	int N=5,S=0;
 	vector<int> arr{-7,-3,-2,5,8};
 	vector<vector<int>> result;
-	result = find_match_case(0,arr);
+	result = find_match_case(S,arr);
 	for(int i=0;i<result.size();i++){
-		cout<<"num of"<< i<<" element case"<<endl;
+		cout<<"num of "<< i<<" element case with sum "<<S<<endl;
 		for (int j=0;j<result[i].size();j++){
 			cout<<result[i][j]<<" ";
-		cout<<endl;
 		}
+		
+		cout<<endl;
 	}
 }
 
