af48fa473b3a296c177cc942245a487d96945155
성봉진,HongGildong skkusosc <tgsbj9807@naver.com>
2019-06-07 18:39:21 +0900

    increased accuracy by using for loop

--- C-Plus-Plus/Math/log_taylor_series.cpp
+++ C-Plus-Plus/Math/log_taylor_series.cpp
@@ -7,9 +7,18 @@ using namespace std;
 double log(double x)
 {
 	double val;
-	val = (x - 1) - (x - 1)*(x - 1) / 2 + (x - 1)*(x - 1)*(x - 1) / 3 - (x - 1)*(x - 1)*(x - 1)*(x - 1) / 4 + (x - 1)*(x - 1)*(x - 1)*(x - 1)*(x - 1) / 5 - (x - 1)*(x - 1)*(x - 1)*(x - 1)*(x - 1)*(x - 1) / 6;
+	double s = x-1;
+	for (int i=1;i<20;i++){
+		val += pow(-1,i)*pow(s,i)/i;
+	}
+
+	//val = (x - 1) - (x - 1)*(x - 1) / 2 + (x - 1)*(x - 1)*(x - 1) / 3 - (x - 1)*(x - 1)*(x - 1)*(x - 1) / 4 + (x - 1)*(x - 1)*(x - 1)*(x - 1)*(x - 1) / 5 - (x - 1)*(x - 1)*(x - 1)*(x - 1)*(x - 1)*(x - 1) / 6;
 
 	return val;
+}
+double pow(double x,double y){
+	if(y==0){return x}
+	else{return x*pow(x,y-1);}
 }
 
 int main(int argc,char**argv)
